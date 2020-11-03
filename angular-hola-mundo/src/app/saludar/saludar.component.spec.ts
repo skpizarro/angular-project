39b515376c17da4ab9f9fb14af41c22d029fa94f
Juan Pablo Pizarro <juanpablopizarro97@gmail.com>
2020-11-03 15:10:28 -0500

    Se creo un nuevo componente

---
+++ angular-hola-mundo/src/app/saludar/saludar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SaludarComponent } from './saludar.component';
+
+describe('SaludarComponent', () => {
+  let component: SaludarComponent;
+  let fixture: ComponentFixture<SaludarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SaludarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SaludarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-hola-mundo/src/app/saludar/saludar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saludar',
+  templateUrl: './saludar.component.html',
+  styleUrls: ['./saludar.component.css']
+})
+export class SaludarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
